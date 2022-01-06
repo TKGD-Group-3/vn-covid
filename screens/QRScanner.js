1740e8591f48ec1ae6abfa490e4dbe1f42507b8b
Bach <50036032+DarkDrake132@users.noreply.github.com>
2022-01-06 12:19:42 +0700

    .

--- screens/QRScanner.js
+++ screens/QRScanner.js
@@ -76,6 +76,7 @@ export default function QRScanner({ navigation }) {
         style={StyleSheet.absoluteFillObject}
       />
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />} */}
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
